cbd053b1013941136a9c6b318493fd5a2c869a2f
ECEEvanCampbell <evan.campbell1@unb.ca>
2021-06-13 21:16:20 -0300

    Added function to keep all random seeds static

---
+++ utils.py
@@ -0,0 +1,13 @@
+import torch
+import numpy as np
+import random
+
+def fix_random_seed(seed_value, use_cuda):
+    np.random.seed(seed_value)  # cpu vars
+    torch.manual_seed(seed_value)  # cpu  vars
+    random.seed(seed_value)  # Python
+    if use_cuda:
+        torch.cuda.manual_seed(seed_value)
+        torch.cuda.manual_seed_all(seed_value)  # gpu vars
+        torch.backends.cudnn.deterministic = True  # needed
+        torch.backends.cudnn.benchmark = False
