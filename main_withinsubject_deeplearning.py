2c0602a2382d9e6ee9e7fb3ffd0aa5f3e44a041a
ECEEvanCampbell <evan.campbell1@unb.ca>
2021-06-23 22:47:16 -0300

    Updated parallel num_workers and pin memory because my PC wouldnt handle it.

--- main_withinsubject_deeplearning.py
+++ main_withinsubject_deeplearning.py
@@ -172,8 +172,11 @@ if __name__ == "__main__":
     # get device available for computation
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     if device.type == "cuda":
-        num_workers = 2
-        pin_memory  = True
+        # My PC sucks, so I can't run things in parallel. If yours can, uncomment these lines
+        #num_workers = 2
+        #pin_memory  = True
+        num_workers = 0
+        pin_memory = False
     else:
         num_workers = 0
         pin_memory  = False
