b1a6f2f139128e69ade3bdc4d9857e67ff3a1334
ECEEvanCampbell <evan.campbell1@unb.ca>
2021-10-18 20:12:36 -0300

    Fixed BN dictionary issue in update_BN_dict method

--- main_subjectindependent_ADANN.py
+++ main_subjectindependent_ADANN.py
@@ -13,6 +13,8 @@ from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 from torch.autograd import Function
 
+import copy
+
 # Define a layer that records the negative gradient for training subject encodings
 class ReversalGradientLayerF(Function):
     @staticmethod
@@ -32,7 +34,7 @@ class ReversalGradientLayerF(Function):
 
 
 class ADANNModel(nn.Module):
-    def __init__(self, n_output, n_channels, input_dims, n_input=64, lambda_value=0.1, num_subjects=10):
+    def __init__(self, n_output, n_channels, input_dims, n_input=64, lambda_value=0.1, num_subjects=10, lr=1e-3):
         super().__init__()
 
         self.lambda_value = lambda_value
@@ -41,9 +43,16 @@ class ADANNModel(nn.Module):
         self.init_network(n_channels, n_input, n_output, input_dims)
 
         self.BN_dict = self.init_BN_dict()
-        # self.switch_BN_dict(1) # Checked, working
-        # self.update_BN_dict(1) # Checked, working
-        # A = 1
+
+        self.class_loss_fn = nn.CrossEntropyLoss()
+        self.domain_loss_fn = nn.CrossEntropyLoss()
+
+        self.optimizer = optim.Adam(self.parameters(), lr = lr)
+        self.scheduler = optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, 'min', threshold=0.02, patience=3, factor=0.2)
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.to(self.device)
+
 
     def init_network(self, n_channels, n_input, n_output, input_dims):
         # How many filters we want
@@ -79,10 +88,10 @@ class ADANNModel(nn.Module):
         for name, param in self.named_parameters():
             if 'bn' in name:
                 # We collect
-                BN_dictionary[name] = param.data.detach().clone()
+                BN_dictionary[name] = copy.deepcopy(param.data[:])
         # repeat dictionary for num_subjects
         for s in range(self.num_subjects):
-            list_BN_dictionary.append(BN_dictionary.copy())
+            list_BN_dictionary.append(copy.deepcopy(BN_dictionary))
 
         return list_BN_dictionary
 
@@ -92,19 +101,19 @@ class ADANNModel(nn.Module):
             if name not in current_state:
                 print('Error in BN state_dict format')
                 raise(KeyError)
-            params = self.BN_dict[subject_id][name].detach().clone() * 5
-            current_state[name].copy_(params)
+            params = copy.deepcopy(self.BN_dict[subject_id][name])
+            self.state_dict()[name][:] = params
     
     def update_BN_dict(self, subject_id):
         current_state = self.state_dict()
         for name in self.BN_dict[subject_id]:
-            self.BN_dict[subject_id][name] = current_state[name]
+            self.BN_dict[subject_id][name] = copy.deepcopy(current_state[name])
 
     def calculate_conv_output_dims(self, input_dims):
         state = torch.zeros(1, *input_dims)
         dims = self.conv1(state)
         dims = self.conv2(dims)
-        dim = self.conv3(dims)
+        dims = self.conv3(dims)
         return int(np.prod(dims.size()))
 
     def forward(self, x):
@@ -128,14 +137,13 @@ class ADANNModel(nn.Module):
         x = self.drop(x)
 
         # Flatten in preparation for linear layer
-        x = x.view(x.size()[0], -1)
+        x_f = x.view(x.size()[0], -1)
        
         # final layer: linear layer that outputs N_Class neurons
-        y = self.fc1(x)
-        y = F.log_softmax(y, dim=2)
+        y = self.fc1(x_f)
 
         # Subject Head - This is where the gradient is reversed to penalize subject specific information
-        reversed_layer = ReversalGradientLayerF.grad_reverse(x, self.lambda_value)
+        reversed_layer = ReversalGradientLayerF.grad_reverse(x_f, self.lambda_value)
         s = self.fc2(reversed_layer)
 
         # Return both class and subject
@@ -143,7 +151,7 @@ class ADANNModel(nn.Module):
 
 
 
-def build_data_loader(batch_size, num_workers, pin_memory, data):
+def build_data_loader(batch_size, num_workers=0, pin_memory=False, data=None):
     data_loader = DataLoader(
         data,
         batch_size=batch_size,
@@ -176,35 +184,79 @@ def pad_sequence(batch):
 # TODO: change from vanilla DL train to ADANN train
 # Class loss & subject loss
 # change BN parameters between subjects
-def train(model, domain_train_loader, antagonistic_train_loader, optimizer, device):
+def train(model, domain_train_loader, antagonistic_train_loader, subjects):
     # Train the model
     # model.train - enable gradient tracking, enable batch normalization, dropout
     model.train()
-    # Store losses of this epoch in a list (element = loss on batch)
-    losses = []
+    # Store losses and accuracies of interest of this epoch in a list (element = loss on batch)
+    loss_domain_class = []
+    loss_domain_subject = []
+    loss_antagonistic_subject = []
+    domain_class_accuracy   = []
+    domain_subject_accuracy = []
+    antagonistic_subject_accuracy = []
 
-    for batch_idx, (data, label) in enumerate(training_loader):
+    # We are now using 2 dataloaders, so we need to advance one in tandem of the other
+    antagonistic_iterable = iter(antagonistic_train_loader)
+    for batch_idx, (domain_data, domain_class) in enumerate(domain_train_loader):
+
+        model.switch_BN_dict(subjects[0])
+        # First, deal with the batch of domain training data 
         # Send data, labels to GPU if GPU is available
-        data = data.to(device)
-        label = label.to(device)
+        domain_data = domain_data.to(model.device)
+        domain_class = domain_class.to(model.device)
+        domain_subject = torch.zeros((domain_data.shape[0]), dtype=torch.long).to(model.device)
+
         # Passing data to model calls the forward method.
-        output = model(data)
-        # Output: (batch_size, 1, n_class)
-        # Use negative log likelihood loss for training
-        loss = F.nll_loss(output.squeeze(), label)
-        # reset optimizer buffer
-        optimizer.zero_grad()
-        # Send the loss to the optimizer (direction of update for each neuron)
-        loss.backward()
-        # Update weights 
-        optimizer.step()
-        # Store the loss of this batch
-        losses.append(loss.item())
+        # Forward returns class logits and domain logits
+        predicted_domain_class, predicted_domain_subject = model(domain_data)
+        
+        # Compute loss for both class prediction and subject prediction of source domain
+        domain_loss_class   = model.class_loss_fn(predicted_domain_class, domain_class)
+        domain_loss_subject = 0.5 * model.domain_loss_fn(predicted_domain_subject, domain_subject)
+
+        # We can compute the backwards update for the classification loss now,
+        # Retain graph is needed here, as domain_loss_subject is not used yet so we can't clear buffers.
+        model.optimizer.zero_grad()
+        domain_loss_class.backward(retain_graph=True)
+        model.optimizer.step()
+
+        # Now we save the BN change from the update
+        model.update_BN_dict(subjects[0])
+
+        # Now we move onto the antagonistic subject
+        # Antagonistic subject -- keep in mind, we only care about the domain loss for the adversarial subject.
+        # We check accuracy here purely for curiosity, antagonistic class labels are not used for weight/BN update
+        model.switch_BN_dict(subjects[1])
+
+        # This should be similar, just set up the required data
+        antagonistic_data, _ = next(antagonistic_iterable)
+        antagonistic_data = antagonistic_data.to(model.device)
+        # The subject labels here are ones to provide a different label than the domain subject
+        antagonistic_subject = torch.ones((antagonistic_data.shape[0]), dtype=torch.long).to(model.device)
+        # Feed the antagonistic data into the model to get subject predictions
+        _, predicted_antagonistic_subject = model(antagonistic_data)
+        # Get domain loss
+        antagonistic_loss_subject = 0.5 * model.domain_loss_fn(predicted_antagonistic_subject, antagonistic_subject)
+
+        loss_domain = 0.1 * (domain_loss_subject + antagonistic_loss_subject)
+        loss_domain.backward()
+        model.optimizer.step()
+
+        loss_domain_class += domain_loss_class
+        loss_domain_subject += domain_loss_subject
+        loss_antagonistic_subject += antagonistic_loss_subject
+        domain_class_accuracy   += ((torch.argmax(predicted_domain_class,dim=1)==domain_class).sum())/predicted_domain_class.shape[0]
+        domain_subject_accuracy += ((torch.argmax(predicted_domain_subject,dim=1)==domain_subject).sum())/predicted_domain_subject.shape[0]
+        antagonistic_subject_accuracy += ((torch.argmax(predicted_antagonistic_subject,dim=1)==antagonistic_subject).sum())/predicted_antagonistic_subject.shape[0]
+        
+
     # Return the average training loss on this epoch
-    return sum(losses)/len(losses)
+    return np.array(loss_domain_class).mean(), np.array(loss_domain_class).mean(), np.array(domain_class_accuracy).mean(), \
+        np.array(domain_subject_accuracy).mean(), np.array(antagonistic_subject_accuracy).mean() 
 
 # TODO: change from vanilla DL validate to ADANN validate
-def validate(model, validation_loader, device):
+def validate(model, domain_valid_loader, antagonistic_valid_loader, device):
     # Evaluate the model
     # model.eval - disable gradient tracking, batch normalization, dropout
     model.eval()
@@ -253,18 +305,6 @@ if __name__ == "__main__":
     # Fix the random seed -- make results reproducible
     # Found in utils.py, this sets the seed for the random, torch, and numpy libraries.
     fix_random_seed(1, torch.cuda.is_available())
-   
-    # get device available for computation
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    if device.type == "cuda":
-        # My PC sucks, so I can't run things in parallel. If yours can, uncomment these lines
-        #num_workers = 2
-        #pin_memory  = True
-        num_workers = 0
-        pin_memory = False
-    else:
-        num_workers = 0
-        pin_memory  = False
 
     # Dataset details, packaged together to easily pass them through functions if required.
     num_subjects  = 10
@@ -293,10 +333,10 @@ if __name__ == "__main__":
     PLOT_LOSS = False
 
     # Start within subject cross-validation scheme
-    # For this example, train with data from all positions from one subject.
-    # Leave one repetition out for cross-validation
-    independent_subject_results = np.zeros((num_subjects))
     # Unlike a standard CNN, we have a loss from the class prediction and a loss from the subject prediction
+    # Domain = source, previous terminology
+    # Antagonistic = adversarial, previous terminology
+    independent_subject_results = np.zeros((num_subjects))
     domain_training_class_loss = np.zeros((num_epochs))
     domain_training_subject_loss = np.zeros((num_epochs))
     domain_validation_class_loss = np.zeros((num_epochs))
@@ -332,14 +372,11 @@ if __name__ == "__main__":
 
     # 
 
-    model = ADANNModel(n_output=num_motions, n_channels=num_channels, num_subjects=num_subjects, input_dims=[num_channels, winsize])
-
-    # send to gpu
-    model.to(device)
+    model = ADANNModel(n_output=num_motions, n_channels=num_channels, num_subjects=num_subjects, input_dims=[num_channels, winsize], lr = lr)
 
     # training setup
-    optimizer = optim.Adam(model.parameters(), lr = lr, weight_decay = weight_decay)
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', threshold=0.02, patience=3, factor=0.2)
+    
+    
     
     # Train process:
     for epoch in range(num_epochs):
@@ -353,24 +390,24 @@ if __name__ == "__main__":
         # __ future work could be to find a method to improve this area while not requiring incredible amounts of memory.
         # These datasets only contain the training data ~ 50% of this dataset
         domain_data_train         = EMGData(domain_subject, chosen_rep_labels=train_rep)
-        domain_train_loader       = build_data_loader(batch_size, num_workers, pin_memory, domain_data_train)
+        domain_train_loader       = build_data_loader(batch_size,  data=domain_data_train)
         antagonistic_data_train   = EMGData(antagonistic_subject, chosen_rep_labels=train_rep)
-        antagonistic_train_loader = build_data_loader(batch_size, num_workers, pin_memory, antagonistic_data_train)
+        antagonistic_train_loader = build_data_loader(batch_size, data=antagonistic_data_train)
         # These datasets only contain the validation data ~ 25% of this dataset
         domain_data_valid         = EMGData(domain_subject, chosen_rep_labels=val_rep)
-        domain_valid_loader       = build_data_loader(batch_size, num_workers, pin_memory, domain_data_valid)
+        domain_valid_loader       = build_data_loader(batch_size, data=domain_data_valid)
         antagonistic_data_valid   = EMGData(antagonistic_subject, chosen_rep_labels=val_rep)
-        antagonistic_valid_loader = build_data_loader(batch_size, num_workers, pin_memory, antagonistic_data_valid)
+        antagonistic_valid_loader = build_data_loader(batch_size, data=antagonistic_data_valid)
 
         domain_training_class_loss[epoch], domain_training_subject_loss[epoch], \
-            antagonistic_training_class_loss[epoch], antagonistic_training_subject_loss[epoch] = train(model, domain_train_loader, antagonistic_train_loader, optimizer, device)
+            antagonistic_training_class_loss[epoch], antagonistic_training_subject_loss[epoch] = train(model, domain_train_loader, antagonistic_train_loader, epoch_subjects)
         domain_validation_class_loss[epoch], domain_validation_subject_loss[epoch], \
-            antagonistic_validation_class_loss[epoch], antagonistic_validation_subject_loss[epoch] = validate(model, domain_valid_loader, antagonistic_valid_loader, device)
+            antagonistic_validation_class_loss[epoch], antagonistic_validation_subject_loss[epoch] = validate(model, domain_valid_loader, antagonistic_valid_loader)
         
         validation_loss = (1-model.lambda_value) * (domain_validation_class_loss + antagonistic_validation_class_loss)/2 + \
             (model.lambda_value) * (domain_validation_subject_loss + antagonistic_validation_subject_loss)/2
 
-        scheduler.step(validation_loss)
+        model.scheduler.step(validation_loss)
         # Add a nice print statement
 
     # Test process:
